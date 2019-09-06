68f1e47a46ea63d87e87ac37b7a87a9352b19c04
chudnofskyATD <mpereannor@gmail.com>
2019-09-06 09:52:02 +0000

    cards.js v1

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,46 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        let cardSection = Card(response.data.articles);
+
+        cardContainer.appendChild(cardSection);
+    })
+    .catch(error => {
+        console.log('Error:', error)
+    })
+
+function Card (cardData){
+
+    //declaration of variables 
+
+    cardDiv = document.createElement('div');
+    headlineDiv = document.createElement('div');
+    author = document.createElement('div');
+    imgContainer = document.createElement('div');
+    img = document.createElement('img');
+    authorNm = document.createElement('span');
+
+    //add contents to elements 
+
+    headlineDiv.texContent = cardData.headline;
+    img.setAttribute('src', cardData.authorPhoto);
+    authorNm.textContent = `By ${cardData.authorName}`;
+
+    //add attributes to elements
+
+    cardDiv.classList.add('card');
+    author.classList.add('author');
+    headlineDiv.classList.add('img-container');
+
+    //fill up parent element
+    
+    cardDiv.appendChild(headlineDiv);
+    cardDiv.appendChild(author);
+    author.appendChild(imgContainer);
+    author.appendChild(authorNm);
+    imgContainer.appendChild(img);
+}
+
+const cardContainer = document.querySelector('.card-container');
